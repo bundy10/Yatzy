e679be29c5c2088c54751995f65e787307c21f77
bundy huang <bundy.huang@myob.com>
2023-01-12 11:54:14 +1100

    first running test

---
+++ Yatzy-Kata/Yatzy-Test/GameTests.cs
@@ -0,0 +1,10 @@
+namespace Yatzy_Test;
+
+public class GameTests
+{
+    [Fact]
+    public void FirstTest()
+    {
+        
+    }
+}
--- Yatzy-Kata/Yatzy-Test/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Yatzy_Test;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
