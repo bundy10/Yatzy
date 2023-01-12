d6c41665ff3dd1f182fcb1611ce0dbfecf1782bd
bundy huang <bundy.huang@myob.com>
2023-01-12 12:16:17 +1100

    Functionality: Game asks player to play again First Test

--- Yatzy-Kata/Yatzy-Kata/Game.cs
+++ Yatzy-Kata/Yatzy-Kata/Game.cs
@@ -1,6 +1,18 @@
+using Yatzy_Kata.Interfaces;
+
 namespace Yatzy_Kata;
 
 public class Game
 {
-    
+    private readonly IPlayer[] _players;
+
+    public Game(IEnumerable<IPlayer> players)
+    {
+        _players = players.ToArray();
+    }
+
+    public void PlayGame()
+    {
+        _players.Any(player => player.PlayAgain());
+    }
 }
--- Yatzy-Kata/Yatzy-Test/GameTests.cs
+++ Yatzy-Kata/Yatzy-Test/GameTests.cs
@@ -1,10 +1,23 @@
+using Moq;
+using Yatzy_Kata;
+using Yatzy_Kata.Interfaces;
+
 namespace Yatzy_Test;
 
 public class GameTests
 {
     [Fact]
-    public void FirstTest()
+    public void GivenPlayGameIsCalled_ThenPromptsPlayerToPlayAgain()
     {
+        //Arrange
+        var playerMock = new Mock<IPlayer>();
+        var game = new Game(new[] { playerMock.Object});
+        playerMock.Setup(player => player.PlayAgain()).Verifiable();
         
+        //Act
+        game.PlayGame();
+        
+        //Assert
+        playerMock.Verify();
     }
 }
